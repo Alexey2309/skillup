bb6d5711363243c99711e4f37d5892022e0a00d6
SkillUP Student <student@skillup.spb.ru>
2018-03-19 22:15:06 +0300

    Lesson 3

--- lesson3.php
+++ lesson3.php
@@ -1,13 +1,33 @@
 <?php
-echo "Hello world!!";
+
+$greeting = 'yess';
+echo "Hello" ." $greeting ". "world!!";
 
 // 2018-03-19 20:11:35
+define('SECONDS_IN_DAY',60*60*24);
+$secondInWeek = SECONDS_IN_DAY *7;
+$nextWeekTime = time() + $secondInWeek;
+echo date('Y-m-d H:i:s',$nextWeekTime);
+echo'<br>' . time() . '<br/>';
 
-echo date('Y-m-d H:i:s',time()+86400*7);
-echo'<br>';
+$thisTime = date('H:i');
+echo $thisTime;
 
-echo time();
+// если сейчас больше 19:00 и меньше 22:00 "занятие идет",
+//Если меньше 22:00 и больше 21:45 - заканчивается
+//иначе не идет.
+//
 
-phpinfo();
-echo '<br>';
+
+if (($thisTime >= '19:00') && ($thisTime < '21:45')) {
+    echo "Занятие идет";
+} elseif (($thisTime <= '22:00') && ($thisTime >= '21:45')) {
+    echo "Заканчивается";
+} else {
+    echo "Не идет";
+}
+
+
+//phpinfo();
+//echo '<br>';
 
