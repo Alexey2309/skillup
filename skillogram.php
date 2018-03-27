4f029b6c8b6e1e30fed217261475a156a9e10277
Alexey2309/skillup.git <git@github.com>
2018-03-28 00:38:06 +0300

    practik

---
+++ skillogram.php
@@ -0,0 +1,107 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <style type="text/css" >
+    BODY {
+     margin-top: 10px;
+        margin-left: 40px;
+        margin-right: 180px;
+    }
+    </style>
+
+    <title>Title</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+    <div class="logo">
+        SKILLOGRAM
+        <span class="styles"><b>По рейтингу</b></span>
+        <span class="styles1"><b>Случайно</b></span>
+
+    </div>
+
+
+
+    <div class="posts-list">
+        <div class="post">
+            <img src="images/Post1.jpg" height="250" width="240"  >
+            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
+        </div>
+        <div class="post">
+            <img src="images/Post2.jpg" height="250" width="240">
+            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
+        </div>
+        <div class="post">
+            <img src="images/Post3.jpg" height="250" width="240">
+            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
+        </div>
+        <div class="post">
+            <img src="images/Post4.jpg" height="250" width="240">
+            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
+        </div>
+        <div class="post">
+            <img src="images/Post5.jpg" height="250" width="240">
+            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
+        </div>
+        <div class="post">
+            <img src="images/Post6.jpg" height="250" width="240">
+            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
+        </div>
+        <div class="post">
+            <img src="images/Post7.jpg" height="250" width="240">
+            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
+        </div>
+        <div class="post">
+            <img src="images/Post8.jpg" height="250" width="240">
+            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
+        </div>
+
+    </div>
+    <div class="clear"></div>
+
+    <!--<img src="/images/logo.png">-->
+
+    <div class="hashtag_wrapper">
+        <a href="#">Neon</a>
+        <a href="#">Aerial</a>
+        <a href="#">Grid</a>
+    </div>
+
+
+    <hr/>  <!-- Horizontal Row (линия) -->
+    <br />   BReak (перенос строки)
+    <img src="https://c1.staticflickвфвфr.com/6/5211/5513402618_3ce232e01a_n.jpуg"
+         alt="Если не удалось загрузить" /> Картинка
+    <br/><br/><br/><br/><br/><br/>
+    <input type="submit" /><br/>
+    Поле ввода / Кнопка
+
+<span class="">
+        <b>
+Bold
+            <i>Italic</i>
+            <u>Underline</u>
+        </b>
+    </span>
+
+    <div>
+        <a href="http://yandex.ru"
+           target="_blank">Ссылка на <span>Яндекс</span></a>
+    </div>
+
+</body>
+</html>
+
