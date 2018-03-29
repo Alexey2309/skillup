f9d468764d9ade35748082eeaddb30a05d67b844
Alexey2309/skillup.git <git@github.com>
2018-03-30 00:22:36 +0300

    tridcatoemarta

--- skillogram.php
+++ skillogram.php
@@ -6,8 +6,7 @@
     <style type="text/css" >
     BODY {
      margin-top: 10px;
-        margin-left: 40px;
-        margin-right: 180px;
+        margin-left: 55px;
     }
     </style>
 
@@ -16,55 +15,105 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-
     <div class="logo">
         SKILLOGRAM
         <span class="styles"><b>По рейтингу</b></span>
         <span class="styles1"><b>Случайно</b></span>
-
     </div>
-
-
-
-    <div class="posts-list">
+    <div class="posts-list" >
         <div class="post">
-            <img src="images/Post1.jpg" height="250" width="240"  >
+            <div class = "post-autor">
+                <img src="images/PhotoAutor_1.jpg">
+            </div>
+            <div class ="nikName">
+                <b>@migli</b><br>
+                <span class="lastTime">два часа назад</span>
+            </div>
+            <img src="images/Post1.jpg" height="290" width="280"  >
+            <br><img src="images/Likes.jpg" height="20" width="40" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="20" width="25" style="margin-left: 212px" >
+        </div>
+        <div class="post">
+            <div class = "post-autor">
+                <img src="images/PhotoAutor_2.jpg">
+            </div>
+            <div class ="nikName">
+                <b>@carovandoe</b><br>
+                <span class="lastTime">три часа назад</span>
+            </div>
+            <img src="images/Post2.jpg" height="290" width="280">
+            <br><img src="images/Likes.jpg" height="20" width="40" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="20" width="25" style="margin-left: 212px" >
+        </div>
+        <div class="post">
+            <div class = "post-autor">
+                <img src="images/PhotoAutor_3.jpg">
+            </div>
+            <div class ="nikName">
+                <b>@fontkiewicz</b><br>
+                <span class="lastTime">два дня назад</span>
+            </div>
+            <img src="images/Post3.jpg" height="290" width="280">
+            <br><img src="images/Likes.jpg" height="20" width="40" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="20" width="25" style="margin-left: 212px" >
+        </div>
+        <div class="post">
+            <div class = "post-autor">
+                <img src="images/PhotoAutor_4.jpg">
+            </div>
+            <div class ="nikName">
+                <b>@ben-staley</b><br>
+                <span class="lastTime">21 час назад</span>
+            </div>
+            <img src="images/Post4.jpg" height="290" width="280">
+            <br><img src="images/Likes.jpg" height="20" width="40" style="margin-top: 5px" >
+            <img src="images/Reposte.jpg" height="20" width="25" style="margin-left: 212px" >
+        </div>
+        <div class="post">
+            <div class = "post-autor">
+                <img src="images/PhotoAutor_5.jpg">
+            </div>
+            <div class ="nikName">
+                <b>@artsalve</b><br>
+                <span class="lastTime">15 секунд назад</span>
+            </div>
+            <img src="images/Post5.jpg" height="290" width="280">
             <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
             <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
         </div>
         <div class="post">
-            <img src="images/Post2.jpg" height="250" width="240">
+            <div class = "post-autor">
+                <img src="images/PhotoAutor_6.jpg">
+            </div>
+            <div class ="nikName">
+                <b>@jimmy-p</b><br>
+                <span class="lastTime">10 минут назад</span>
+            </div>
+            <img src="images/Post6.jpg" height="290" width="280">
             <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
             <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
         </div>
         <div class="post">
-            <img src="images/Post3.jpg" height="250" width="240">
+            <div class = "post-autor">
+                <img src="images/PhotoAutor_7.jpg">
+            </div>
+            <div class ="nikName">
+                <b>@richardgurtler</b><br>
+                <span class="lastTime">18 часов назад</span>
+            </div>
+            <img src="images/Post7.jpg" height="290" width="280">
             <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
             <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
         </div>
         <div class="post">
-            <img src="images/Post4.jpg" height="250" width="240">
-            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
-            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
-        </div>
-        <div class="post">
-            <img src="images/Post5.jpg" height="250" width="240">
-            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
-            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
-        </div>
-        <div class="post">
-            <img src="images/Post6.jpg" height="250" width="240">
-            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
-            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
-        </div>
-        <div class="post">
-            <img src="images/Post7.jpg" height="250" width="240">
-            <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
-            <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
-        </div>
-        <div class="post">
-            <img src="images/Post8.jpg" height="250" width="240">
+            <div class = "post-autor">
+                <img src="images/PhotoAutor_8.jpg">
+            </div>
+            <div class ="nikName">
+                <b>@inconvergent</b><br>
+                <span class="lastTime">вчера</span>
+            </div>
+            <img src="images/Post8.jpg" height="290" width="280">
             <br><img src="images/Likes.jpg" height="15" width="35" style="margin-top: 5px" >
             <img src="images/Reposte.jpg" height="15" width="20" style="margin-left: 182px" >
         </div>
