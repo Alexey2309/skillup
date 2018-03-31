aff77db969b634f777823ecac2211c7a30ba8481
Alexey2309/skillup.git <git@github.com>
2018-03-31 20:33:51 +0300

    part2

---
+++ homeWork/futear.php
@@ -0,0 +1,23 @@
+
+<body>
+<div class="futerFon">
+    <div class="nameCompany">
+Developer 2017
+</div>
+    <div class="info">
+        <span><b>О проекте</b></span>
+        <span style="margin-left: 50px"><b>Помощь</b></span>
+        <span style="margin-left: 50px"><b>Поддержать проект</b></span>
+    </div>
+    <div class="subscribe">
+Подписаться
+    </div>
+    <div class="e-maile">
+Ваш e-mail, чтобы получать вдохновение
+</div>
+
+</div>
+<div class="clear"></div>
+
+</body>
+</html>
--- homeWork/skillogram.php
+++ homeWork/skillogram.php
@@ -207,28 +207,10 @@ require('header.php');
             <span class ="tags">#foods</span>
         </div>
     </div>
-
 </div>
-<div class="futerFon">
-    <div class="nameCompany">
-        Developer 2017
-    </div>
-    <div class="info">
-        <span><b>О проекте</b></span>
-        <span style="margin-left: 50px"><b>Помощь</b></span>
-        <span style="margin-left: 50px"><b>Поддержать проект</b></span>
-    </div>
-    <div class="subscribe">
-        Подписаться
-    </div>
-    <div class="e-maile">
-        Ваш e-mail, чтобы получать вдохновение
-    </div>
-
-</div>
-<div class="clear"></div>
+<?php
+require('futear.php');
+?>
 
 
-</body>
-</html>
 
